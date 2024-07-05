535f3cd22fb2e3f9063ed5d65646ac43097e0f62
laxman67 <laxmanhenricks94@gmail.com>
2024-07-05 22:41:52 +0530

    refactor

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -13,9 +13,9 @@ const StoreContextProvider = (props) => {
 
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
-      setCartItems({ ...cartItems, [itemId]: 1 }));
+      setCartItems({ ...cartItems, [itemId]: 1 });
     } else {
-      setCartItems({ ...cartItems, [itemId]: cartItems[itemId] + 1 }));
+      setCartItems({ ...cartItems, [itemId]: cartItems[itemId] + 1 });
     }
   };
 
