8168b16c3bc6c291eb2314ee41733c119aff916a
laxman67 <laxmanhenricks94@gmail.com>
2024-07-05 09:04:02 +0530

    food item - cart icons

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,10 +1,35 @@
+import { useState } from 'react';
 import { assets } from '../../assets/assets';
 import './FoodItem.css';
-function FoodItem({ _id, name, price, description, image }) {
+// eslint-disable-next-line react/prop-types
+function FoodItem({ id, name, price, description, image }) {
+  const [itemCount, setItemCount] = useState(0);
   return (
     <div className="food-item">
       <div className="food-item-img-container">
         <img src={image} className="food-item-image" alt={name} />
+        {!itemCount ? (
+          <img
+            className="add"
+            onClick={() => setItemCount((prev) => prev + 1)}
+            src={assets.add_icon_white}
+            alt=""
+          />
+        ) : (
+          <div className="food-item-counter">
+            <img
+              onClick={() => setItemCount((prev) => prev - 1)}
+              src={assets.remove_icon_red}
+              alt=""
+            />
+            <p>{itemCount}</p>
+            <img
+              onClick={() => setItemCount((prev) => prev + 1)}
+              src={assets.add_icon_green}
+              alt=""
+            />
+          </div>
+        )}
       </div>
       <div className="food-item-info">
         <div className="food-item-name-rating">
