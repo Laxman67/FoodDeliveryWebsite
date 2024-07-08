df9c783b74fa085c35d7bd651a1d145ea381b287
laxman67 <laxmanhenricks94@gmail.com>
2024-07-09 00:10:32 +0530

    LoginPopup Styling

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,12 +1,12 @@
-import { useState } from "react";
-import "./LoginPopup.css";
-import { assets } from "../../assets/assets";
+import { useState } from 'react';
+import './LoginPopup.css';
+import { assets } from '../../assets/assets';
 
 const LoginPopup = ({ setShowLogin }) => {
-  const [currState, setCurrState] = useState("Login");
+  const [currState, setCurrState] = useState('Login');
   return (
     <div className="login-popup">
-      <form action="" method="post" className="login-popup-container">
+      <form action="/" method="post" className="login-popup-container">
         <div className="login-popup-title">
           <h2>{currState}</h2>
           <img
@@ -16,7 +16,7 @@ const LoginPopup = ({ setShowLogin }) => {
           />
         </div>
         <div className="login-popup-input">
-          {currState == "Login" ? (
+          {currState == 'Login' ? (
             <></>
           ) : (
             <input type="text" placeholder="Your name" required />
@@ -25,20 +25,20 @@ const LoginPopup = ({ setShowLogin }) => {
           <input type="email" placeholder="Your Email" required />
           <input type="password" placeholder="Password" required />
         </div>
-        <button>{currState === "Sign Up" ? "Create account" : "Login"}</button>
+        <button>{currState === 'Sign Up' ? 'Create account' : 'Login'}</button>
         <div className="login-popup-condition">
           <input type="checkbox" required />
           <p>By Continuing , I agree to the terma and condition</p>
         </div>
-        {currState === "Login" ? (
+        {currState === 'Login' ? (
           <p>
-            Create a new Account?{" "}
-            <span onClick={() => setCurrState("Sign Up")}>Click Here</span>
+            Create a new Account?
+            <span onClick={() => setCurrState('Sign Up')}> Click Here</span>
           </p>
         ) : (
           <p>
-            Already have an acoount ?{" "}
-            <span onClick={() => setCurrState("Login")}>Login here</span>
+            Already have an acoount ?
+            <span onClick={() => setCurrState('Login')}> Login here</span>
           </p>
         )}
       </form>
